004e3a1e49ab638f1a3166a56415d1715f362c5b
rolandtcs <rolandtcs@hotmail.com>
2025-10-12 02:44:56 +0800

    rollback

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { SpeedInsights } from "@vercel/speed-insights/react";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>
 );
